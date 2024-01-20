835da3103fd019162d99a7b27094a25ae406eadb
suman chapai <sumanchapai@gmail.com>
2024-01-20 14:33:40 +0545

    Add function to append string to file after a given magic string

--- cmd/import.go
+++ cmd/import.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"strings"
@@ -38,4 +39,30 @@ func AddSchemaImportStatement(path string, name string) error {
 	}
 	// Append the import string to the document
 	return nil
+}
+
+// Add stringToAdd to filename after a given magic string. Returns any error encountered.
+// The way this function works is that it reads the entire file into memory, splits the file based
+// on the given magic string, and joins the file using the magic string
+func AddToFileAfterMagicString(filename string, magic string, stringToAdd string) error {
+	magicBytes := []byte(magic)
+	// Read file into a buffer
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	result := bytes.Split(b, magicBytes)
+	if noOfOccurances := len(result) - 1; noOfOccurances != 1 {
+		return fmt.Errorf("expected magic string %v to be present once. found %v times", magic, noOfOccurances)
+	}
+	// Contains magic string exactly once
+	// Add the string to right after the magic string
+	result[1] = append([]byte(stringToAdd), result[1]...)
+	// Expect magic string to end with newline
+	if magicBytes[len(magicBytes)-1] != '\n' {
+		return fmt.Errorf("expected magic string to end with newline got %q", magic)
+	}
+	dataToWrite := bytes.Join(result, magicBytes)
+	err = os.WriteFile(filename, dataToWrite, 0o644)
+	return err
 }
