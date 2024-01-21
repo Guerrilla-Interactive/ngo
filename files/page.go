6bacdb95dfad70e60b342d0c37106e8150d32122
suman chapai <sumanchapai@gmail.com>
2024-01-21 18:41:20 +0545

    Enhance page.tsx template. Avoid inferable query type

--- files/page.go
+++ files/page.go
@@ -6,12 +6,9 @@ import "text/template"
 const page = `import { draftMode } from 'next/headers'
 import { notFound } from "next/navigation"
 
-import type { ZodType } from 'zod'
-
 import { serverEnv } from '@/lib/env/server'
 import { runQuery } from '@/sanity/groqd-client'
 
-import type { {{.PascalCaseComponentName}}IndexQuery} from '../({{.KebabCaseComponentName}}-index-server)/{{.KebabCaseComponentName}}.index-query';
 import { {{.CamelCaseComponentName}}IndexQuery } from '../({{.KebabCaseComponentName}}-index-server)/{{.KebabCaseComponentName}}.index-query'
 import {{.PascalCaseComponentName}}IndexBody from './{{.KebabCaseComponentName}}.index-component'
 import { {{.PascalCaseComponentName}}Preview} from './{{.KebabCaseComponentName}}.index-preview'
@@ -19,7 +16,7 @@ import { {{.PascalCaseComponentName}}Preview} from './{{.KebabCaseComponentName}
 const {{.PascalCaseComponentName}}IndexRoute = async () => {
   const { isEnabled: draftModeEnabled } = draftMode()
   const token = serverEnv.SANITY_API_READ_TOKEN
-  const data = await runQuery<ZodType<{{.PascalCaseComponentName}}IndexQuery | null>>(
+  const data = await runQuery(
     {{.CamelCaseComponentName}}IndexQuery,
     {},
     draftModeEnabled ? token : undefined
--- files/slug_page.go
+++ files/slug_page.go
@@ -5,11 +5,10 @@ import "text/template"
 // page.tsx for dynamic page
 const slugPage = `import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
-import type { ZodType } from 'zod'
 
 import { serverEnv } from '@/lib/env/server'
 import { runQuery } from '@/sanity/groqd-client'
-import { {{.PascalCaseComponentName}}SlugQuery, {{.CamelCaseComponentName}}SlugQuery } from "../({{.KebabCaseComponentName}}-slug-server)/{{.KebabCaseComponentName}}.slug-query"
+import { {{.CamelCaseComponentName}}SlugQuery } from "../({{.KebabCaseComponentName}}-slug-server)/{{.KebabCaseComponentName}}.slug-query"
 import { Preview{{.PascalCaseComponentName}}Slug } from "./{{.KebabCaseComponentName}}.slug-preview"
 import {{.PascalCaseComponentName}}SlugBody  from "./{{.KebabCaseComponentName}}.slug-component"
 
@@ -27,7 +26,7 @@ const {{.PascalCaseComponentName}}SlugRoute = async ({ params }: PageProps) => {
   const { isEnabled: draftModeEnabled } = draftMode()
   const token = serverEnv.SANITY_API_READ_TOKEN
 
-  const data = await runQuery<ZodType<ProductsSlugQuery | null>>(
+  const data = await runQuery(
 		{{.CamelCaseComponentName}}SlugQuery,
 		{ slug: params.slug },
 		draftModeEnabled ? token : undefined
