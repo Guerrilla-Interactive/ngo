c96e21f9ede756db3b2ef7d67aa891a52e9361d9
suman chapai <sumanchapai@gmail.com>
2024-01-24 10:33:46 +0545

    Fix template for catchall optional dynamic routes

--- files/slug_page_catchall_optional.go
+++ files/slug_page_catchall_optional.go
@@ -13,8 +13,8 @@ import { Preview{{.PascalCaseComponentName}}Slug } from "./{{.KebabCaseComponent
 import {{.PascalCaseComponentName}}SlugBody from "./{{.KebabCaseComponentName}}.slug-component"
 
 
-interface PageParams extends Record<string, any> {
-	slug: string
+interface PageParams extends Record<string, string | Array<string>> {
+	slug: Array<string>
 }
 
 interface PageProps {
@@ -26,17 +26,25 @@ const {{.PascalCaseComponentName}}SlugRoute = async ({ params }: PageProps) => {
   const { isEnabled: draftModeEnabled } = draftMode()
   const fetchClient = draftModeEnabled ? runDraftQuery : runQuery
 
+  // Note this must be placed before accessing the first element of parms.slug in the catch all route
+  // This is because this is an optional catchall route. See details here:
+  // https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes
+  if (!params?.slug || params?.slug?.length == 0) {
+		return <div>THIS IS AN OPTIONAL CATCHALL ROUTE. WRITE YOUR LOGIC TO HANDLE NO SLUG PARARMS HERE</div>
+  }
+
   const data = await fetchClient(
 		{{.CamelCaseComponentName}}SlugQuery,
-		{ slug: params.slug }
+		{ slug: params.slug[0] }
   )
 
   if (!data) {
 	return notFound()
   }
+  
 
   if (draftModeEnabled) {
-	  return <Preview{{.PascalCaseComponentName}}Slug initial={data} />
+	  return <Preview{{.PascalCaseComponentName}}Slug initial={data} queryParams={params} slug={params.slug[0]!} />
   }
 
   return <{{.PascalCaseComponentName}}SlugBody data={data} />
