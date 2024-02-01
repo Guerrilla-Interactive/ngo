6c114b6c3069d93c2ea9a054bce8204e7c629697
suman chapai <sumanchapai@gmail.com>
2024-02-01 22:21:59 +0545

    lint: update printf usage

--- cmd/version.go
+++ cmd/version.go
@@ -26,7 +26,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Printf(version)
+		fmt.Printf("%v", version)
 	},
 }
 
